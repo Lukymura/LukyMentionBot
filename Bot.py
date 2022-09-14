86757f78dc6bfe1476a90654170eacbf7a3c7a56
Lukymura <lukyheroku@gmail.com>
2022-09-14 14:51:39 +0700

    LukyMentionBot

---
+++ Bot.py
@@ -0,0 +1,234 @@
+# A Powerful Music And Management Bot
+# Property Of Rocks Indian Largest Chatting Group
+# Rocks © @Dr_Asad_Ali © Rocks
+# Owner Asad Ali + Harshit Sharma + Abhimanyu Singh + Krishna Ki Diwani
+
+
+import os, logging, asyncio
+from telethon import Button
+from telethon import TelegramClient, events
+from telethon.tl.types import ChannelParticipantAdmin
+from telethon.tl.types import ChannelParticipantCreator
+from telethon.tl.types import ChannelParticipantsAdmins
+from telethon.tl.functions.channels import GetParticipantRequest
+from telethon.errors import UserNotParticipantError
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+
+logging.basicConfig(
+    level=logging.INFO, format="%(name)s - [%(levelname)s] - %(message)s"
+)
+LOGGER = logging.getLogger(__name__)
+
+api_id = int(os.environ.get("APP_ID", ""))
+api_hash = os.environ.get("API_HASH", "")
+bot_token = os.environ.get("TOKEN", "")
+client = TelegramClient("client", api_id, api_hash).start(bot_token=bot_token)
+spam_chats = []
+
+
+@client.on(events.NewMessage(pattern="^/start$"))
+async def start(event):
+    chat_id = event.chat_id
+    if not event.is_private:
+        return await event.respond("ɪ ᴀᴍ ᴀʟɪᴠᴇ")
+    await event.reply(
+        "━━━━━━━━━━━━━━━━━━━━━━━━\n\nɪ ᴀᴍ ʟᴜᴋʏ ᴛᴏ ᴍᴇɴᴛɪᴏɴ ᴀʟʟ ᴛʜᴇ ɢʀᴏᴜᴘ ᴍᴇᴍʙᴇʀs ɪɴ ᴛᴇʟᴇɢʀᴀᴍ\n ᴛʜᴀɴᴋs ғᴏʀ ᴜsɪɴɢ ʀᴜɴ /help..\n\nɪғ ʏᴏᴜ ʜᴀᴠᴇ ᴀɴʏ ǫᴜᴇsᴛɪᴏɴs ᴛʜᴇɴ\nᴅᴍ ᴛᴏ ᴍʏ [ᴏᴡɴᴇʀ](https://t.me/KyMuraa) ...\n\n━━━━━━━━━━━━━━━━━━━━━━━━",
+        link_preview=False,
+        buttons=(
+            [
+                Button.url(
+                    "ᴍᴀsᴜᴋɪɴ ɢᴜᴇ ᴋᴇ ɢᴄ ʟᴜ︎︎",
+                    "https://t.me/Lukytapibot?startgroup=true",
+                ),
+            ],
+            [
+                Button.url("ɢʀᴏᴜᴘ︎︎︎", "https://t.me/Shayri_Music_Lovers"),
+                Button.url("ᴄʜᴀɴɴᴇʟ︎︎", "https://t.me/AsadSupport"),
+            ],
+            [
+                Button.url("ᴀʏᴀɴɢ︎︎️️", "https://t.me/Ayyaaaxn),
+                Button.url("ʟᴜᴋʏ ɢᴀɴᴛᴇɴɢ", "https://t.me/KyMuraa"),
+            ],
+        ),
+    )
+
+
+@client.on(events.NewMessage(pattern="^/help$"))
+async def help(event):
+    chat_id = event.chat_id
+    if not event.is_private:
+        return await event.respond("ᴅᴇᴀʀ sᴛᴀʀᴛ ᴍᴇ ɪɴ ᴘᴍ ᴛᴏ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ")
+    helptext = "✪ ʜᴇʟᴘ ᴍᴇɴᴜ ᴏғ ʟᴜᴋʏ ᴍᴇɴᴛɪᴏɴ\n\n✪ ᴄᴏᴍᴍᴀɴᴅ: /mentionall\n✪ ᴄᴏᴍᴍᴀɴᴅ: /cancel ᴛᴏ ᴄᴀɴᴄᴇʟ ɢᴏɪɴɢ ᴏɴ ᴘʀᴏᴄᴇss.\n✪ ᴄᴏᴍᴍᴀɴᴅ /admin ᴛᴏ ᴍᴇɴᴛɪᴏɴ ᴀʟʟ ᴀᴅᴍɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ\n✪ Yᴏᴜ ᴄᴀɴ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ᴡɪᴛʜ ᴛᴇxᴛ ᴡʜᴀᴛ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴍᴇɴᴛɪᴏɴ ᴏᴛʜᴇʀs.\n✪ `Example: /mentionall Good Morning!`\n✪ Yᴏᴜ ᴄᴀɴ ʏᴏᴜ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ᴀs ᴀ ʀᴇᴘʟʏ ᴛᴏ ᴀɴʏ ᴍᴇssᴀɢᴇ. Bᴏᴛ ᴡɪʟʟ ᴛᴀɢ ᴜsᴇʀs ᴛᴏ ᴛʜᴀᴛ ʀᴇᴘʟɪᴇᴅ ᴍᴇsssᴀɢᴇ."
+    await event.reply(
+        helptext,
+        link_preview=False,
+        buttons=(
+            [
+                Button.url("ɢᴀʙᴜᴛ", "https://t.me/Kyyomasaa"),
+                Button.url("ᴋʟɪᴋ", "www.xnxx.com"),
+            ]
+        ),
+    )
+
+
+@client.on(events.NewMessage(pattern="^/owner$"))
+async def help(event):
+    chat_id = event.chat_id
+    if not event.is_private:
+        return await event.respond("ᴅᴇᴀʀ sᴛᴀʀᴛ ᴍᴇ ɪɴ ᴘᴍ ᴛᴏ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ")
+    helptext = "✪ ᴏᴡɴᴇʀ ᴍᴇɴᴜ ᴏғ ʟᴜᴋʏ ᴍᴇɴᴛɪᴏɴ\n\n✪ ᴍʏ ᴏᴡɴᴇʀ ɪs [ʟᴜᴋʏ](https://t.me/KyMuraa)\n✪ ғᴜᴛᴜʀᴇ ᴀɴᴇsᴛʜᴇᴛɪᴄ."
+    await event.reply(
+        helptext,
+        link_preview=False,
+        buttons=(
+            [
+                Button.url("ɢᴀʙᴜᴛ", "https://t.me/Kyyomasaa"),
+                Button.url("ᴋʟɪᴋ", "www.xnxx.com"),
+            ]
+        ),
+    )
+
+
+@client.on(events.NewMessage(pattern="^/mentionall ?(.*)"))
+async def mentionall(event):
+    chat_id = event.chat_id
+    if event.is_private:
+        return await event.respond(
+            "__This command can be use in groups and channels!__"
+        )
+
+    is_admin = False
+    try:
+        partici_ = await client(GetParticipantRequest(event.chat_id, event.sender_id))
+    except UserNotParticipantError:
+        is_admin = False
+    else:
+        if isinstance(
+            partici_.participant, (ChannelParticipantAdmin, ChannelParticipantCreator)
+        ):
+            is_admin = True
+    if not is_admin:
+        return await event.respond("__Only admins can mention all!__")
+
+    if event.pattern_match.group(1) and event.is_reply:
+        return await event.respond("__Give me one argument!__")
+    elif event.pattern_match.group(1):
+        mode = "text_on_cmd"
+        msg = event.pattern_match.group(1)
+    elif event.is_reply:
+        mode = "text_on_reply"
+        msg = await event.get_reply_message()
+        if msg == None:
+            return await event.respond(
+                "__I can't mention members for older messages! (messages which are sent before I'm added to group)__"
+            )
+    else:
+        return await event.respond(
+            "__Reply to a message or give me some text to mention others!__"
+        )
+
+    spam_chats.append(chat_id)
+    usrnum = 0
+    usrtxt = ""
+    async for usr in client.iter_participants(chat_id):
+        if not chat_id in spam_chats:
+            break
+        usrnum += 1
+        usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
+        if usrnum == 5:
+            if mode == "text_on_cmd":
+                txt = f"{usrtxt}\n\n{msg}"
+                await client.send_message(chat_id, txt)
+            elif mode == "text_on_reply":
+                await msg.reply(usrtxt)
+            await asyncio.sleep(2)
+            usrnum = 0
+            usrtxt = ""
+    try:
+        spam_chats.remove(chat_id)
+    except:
+        pass
+
+
+@client.on(events.NewMessage(pattern="^/admins|/admin|@admin|@admins ?(.*)"))
+async def _(event):
+    chat_id = event.chat_id
+    if event.is_private:
+        return await event.respond("sᴏʀʀʏ ʏᴏᴜ ᴄᴀɴ ᴍᴇɴᴛɪᴏɴ ᴀᴅᴍɪɴ ᴏɴʟʏ ɪɴ ɢʀᴏᴜᴘ")
+
+    is_admin = False
+    try:
+        partici_ = await client(GetParticipantRequest(event.chat_id, event.sender_id))
+    except UserNotParticipantError:
+        is_admin = False
+    else:
+        if isinstance(
+            partici_.participant, (ChannelParticipantAdmin, ChannelParticipantCreator)
+        ):
+            is_admin = True
+    if not is_admin:
+        return await event.respond("ᴏɴʟʏ ᴀᴅᴍɪɴ ᴄᴀɴ ᴍᴇɴᴛɪᴏɴ ɢʀᴏᴜᴘ ᴀᴅᴍɪɴs")
+
+    if event.pattern_match.group(1) and event.is_reply:
+        return await event.respond("ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ ᴍᴇɴᴛɪᴏɴ")
+    elif event.pattern_match.group(1):
+        mode = "text_on_cmd"
+        msg = event.pattern_match.group(1)
+    elif event.is_reply:
+        mode = "text_on_reply"
+        msg = await event.get_reply_message()
+        if msg == None:
+            return await event.respond(
+                "__ɪ ᴄᴀɴ'ᴛ ᴍᴇɴᴛɪᴏɴ ᴍᴇᴍʙᴇʀs ꜰᴏʀ ᴏʟᴅᴇʀ ᴍᴇssᴀɢᴇs! (ᴍᴇssᴀɢᴇs ᴡʜɪᴄʜ ᴀʀᴇ sᴇɴᴛ ʙᴇꜰᴏʀᴇ ɪ'ᴍ ᴀᴅᴅᴇᴅ ᴛᴏ ɢʀᴏᴜᴘ)__"
+            )
+    else:
+        return await event.respond(
+            "__ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴏʀ ɢɪᴠᴇ ᴍᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ ᴍᴇɴᴛɪᴏɴ ᴏᴛʜᴇʀs!__"
+        )
+
+    spam_chats.append(chat_id)
+    usrnum = 0
+    usrtxt = ""
+    chat = await event.get_input_chat()
+    async for x in client.iter_participants(chat, filter=ChannelParticipantsAdmins):
+        if not chat_id in spam_chats:
+            break
+        usrnum += 1
+        usrtxt += f" \n [{x.first_name}](tg://user?id={x.id})"
+        if usrnum == 5:
+            if mode == "text_on_cmd":
+                txt = f"{usrtxt}\n\n{msg}"
+                await client.send_message(chat_id, txt)
+            elif mode == "text_on_reply":
+                await msg.reply(usrtxt)
+            await asyncio.sleep(2)
+            usrnum = 0
+            usrtxt = ""
+    try:
+        spam_chats.remove(chat_id)
+    except:
+        pass
+
+
+@client.on(events.NewMessage(pattern="^/cancel$"))
+async def cancel_spam(event):
+    if not event.chat_id in spam_chats:
+        return await event.respond("__There is no proccess on going...__")
+    else:
+        try:
+            spam_chats.remove(event.chat_id)
+        except:
+            pass
+        return await event.respond("__Stopped.__")
+
+
+print(">> LUKY WORKING <<")
+client.run_until_disconnected()
+
+
+# A Powerful Music And Management Bot
+# Property Of Rocks Indian Largest Chatting Group
+# Rocks © @Dr_Asad_Ali © Rocks
+# Owner Asad Ali + Harshit Sharma + Abhimanyu Singh + Krishna Ki Diwani
---
+++ Readme.md
@@ -0,0 +1,31 @@
+<h1 align="center"><b> LUKY MENTION BOT ️</b></h1>
+
+<h4 align="center">Luky Mention A Powerful, Smart And Simple And Smart Mention Bot <br> ... Written With Python and Telethon...</h4>
+
+<p align="center"><a href="https://t.me/Kyyomasaa"><img src="https://telegra.ph/file/fee19afb12da12d389fc3.jpg" width="400"></a></p>
+
+<p align="center">
+    <a href="https://app.codacy.com/manual/Lukymura/LukyMentionBot/dashboard"> <img src="https://img.shields.io/codacy/grade/4d58f2a402b54aed8a7d95f7add45a81?color=brightgreen&logo=codacy&logoColor=green&style=for-the-badge" alt="Codacy" /></a>
+    <a href="https://github.com//LukyMentionBot"> <img src="https://img.shields.io/github/repo-size/Lukymura/LukyMentionBot?color=orange&logo=github&logoColor=green&style=for-the-badge" /></a>
+    <a href="https://github.com/Lukymura/LukyMentionBot/commits/prince"> <img src="https://img.shields.io/github/last-commit/Lukymura/LukyMentionBot?color=brown&logo=github&logoColor=green&style=for-the-badge" /></a>
+    <a href="https://github.com/Lukymura/LukyMentionBot/issues"> <img src="https://img.shields.io/github/issues/Lukymura/LukyMentionBot?color=blueviolet&logo=github&logoColor=green&style=for-the-badge" /></a>
+    <a href="https://github.com/Lukymura/LukyMentionBot/network/members"> <img src="https://img.shields.io/github/forks/Lukymura/LukyMentionBot?color=red&logo=github&logoColor=green&style=for-the-badge" /></a>  
+    <a href="https://pypi.org/project/Telethon/"> <img src="https://img.shields.io/pypi/v/telethon?color=yellow&label=telethon&logo=python&logoColor=green&style=for-the-badge" /></a>
+</p>
+
+> ⭐️ Thanks to everyone for using Luky, That is the greatest pleasure we have !
+
+## Avaiilable on Telegram as [@Kyyomasaa](https://t.me/Kyyomasaa)
+
+## ✨ Easy To Deploy Direct In Repo✨
+
+The Easiest Way To Deploy This Bot Direct From GitHub
+
+<p align="center"><a href="https://heroku.com/deploy"><img src="https://www.herokucdn.com/deploy/button.svg"></a>
+
+## ✨ Easy To Deploy Direct In Repo✨
+
+The Easiest Way To Deploy This On Heroku For Any Browser Copy Link Supported
+
+<p align="center"><a href="https://heroku.com/deploy?template=https://github.com/Lukymura/LukyMentionBot"> <img src="https://img.shields.io/badge/Deploy%20To%20Heroku-black?style=for-the-badge&logo=heroku" width="220" height="38.45"/></a></p>
+ 
